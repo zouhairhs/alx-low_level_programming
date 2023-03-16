b9c3462d528047feed3d879d4c826954103598c2
zouhairhs <elhoussni3@gmail.com>
2023-03-16 16:10:34 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,6 @@
  *  main - A programme that prints a line using the printf function
  *  Return: 0 (Success)
  */
-
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n")
