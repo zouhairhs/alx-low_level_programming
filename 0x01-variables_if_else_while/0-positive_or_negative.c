92b7e2dd51f0f5bb8aa6464464c2e0b40991620a
zouhairhs <elhoussni3@gmail.com>
2023-03-17 09:41:28 -0700

    #include <stdio.h> #include <stdlib.h> #include <time.h> /**  * main - Entry point  *  * Return: Always 0  */ int main(void) { int n;
    srand(time(0));
    n = rand() - RAND_MAX / 2;
    if  (n > 0)
    printf("%d is positive\n", n);
    else if (n < 0)
    printf("%d is negative\n", n);
    else
    printf("%d is zero\n", n);
    return (0);
    }

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,22 +1,22 @@
-#include <stdio.h>
+[200~#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0
+ *     */
+int main(void)
+{
+		int n;
 
-int main(void) 
-{
-    int n;
-    srand(time(NULL));  // Seed the random number generator with the current time
-    n = rand() % 201 - 100;  // Generate a random integer between -100 and 100
-    printf("The number %d is ", n);
-    if (n > 0)
-{
-        printf("positive.\n");
-} else if (n < 0) 
-{
-        printf("negative.\n");
-} else 
-{
-        printf("zero.\n");
-}
-    return (0);
+			srand(time(0));
+				n = rand() - RAND_MAX / 2;
+				if  (n > 0)
+					printf("%d is positive\n", n);
+				else if (n < 0)
+					printf("%d is negative\n", n);
+				else
+					printf("%d is zero\n", n);
+				return (0);
 }
